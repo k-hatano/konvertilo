a16680c3dcd18ab0ae0ada2ec0d690feac9269df
kenta.hatano <kenta.hatano@s-cubism.jp>
2019-03-12 16:48:31 +0900

    カタカナイズ機能追加（必要か？）

--- konvertilo.js
+++ konvertilo.js
@@ -339,6 +339,187 @@ function konvertuAlAscii() {
 	document.getElementById('enhavo').value = enhavo;
 }
 
+function katakanigo() {
+	var enhavo = document.getElementById('enhavo').value;
+
+	enhavo = enhavo.replace(/[Bb][Aa]/g,'バ');
+	enhavo = enhavo.replace(/[Bb][Ii]/g,'ベ');
+	enhavo = enhavo.replace(/[Bb][Uu]/g,'ビ');
+	enhavo = enhavo.replace(/[Bb][Ee]/g,'ボ');
+	enhavo = enhavo.replace(/[Bb][Oo]/g,'ブ');
+	enhavo = enhavo.replace(/[Bb]/g,'ㇷ゛');
+
+	enhavo = enhavo.replace(/[Cc][Aa]/g,'ツァ');
+	enhavo = enhavo.replace(/[Cc][Ii]/g,'ツィ');
+	enhavo = enhavo.replace(/[Cc][Uu]/g,'ツ');
+	enhavo = enhavo.replace(/[Cc][Ee]/g,'ツェ');
+	enhavo = enhavo.replace(/[Cc][Oo]/g,'ツォ');
+	enhavo = enhavo.replace(/[Cc]/g,'ッ');
+
+	enhavo = enhavo.replace(/[Ĉĉ][Aa]/g,'チャ');
+	enhavo = enhavo.replace(/[Ĉĉ][Ii]/g,'チ');
+	enhavo = enhavo.replace(/[Ĉĉ][Uu]/g,'チュ');
+	enhavo = enhavo.replace(/[Ĉĉ][Ee]/g,'セ゚');
+	enhavo = enhavo.replace(/[Ĉĉ][Oo]/g,'チョ');
+	enhavo = enhavo.replace(/[Ĉĉ]/g,'ッ');
+
+	enhavo = enhavo.replace(/[Dd][Aa]/g,'ダ');
+	enhavo = enhavo.replace(/[Dd][Ii]/g,'ディ');
+	enhavo = enhavo.replace(/[Dd][Uu]/g,'ドゥ');
+	enhavo = enhavo.replace(/[Dd][Ee]/g,'デ');
+	enhavo = enhavo.replace(/[Dd][Oo]/g,'ド');
+	enhavo = enhavo.replace(/[Dd]/g,'ㇳ゛');
+
+	enhavo = enhavo.replace(/[Ff][Aa]/g,'ファ');
+	enhavo = enhavo.replace(/[Ff][Ii]/g,'フィ');
+	enhavo = enhavo.replace(/[Ff][Uu]/g,'フ');
+	enhavo = enhavo.replace(/[Ff][Ee]/g,'フェ');
+	enhavo = enhavo.replace(/[Ff][Oo]/g,'フォ');
+	enhavo = enhavo.replace(/[Ff]/g,'ㇷ');
+
+	enhavo = enhavo.replace(/[Gg][Aa]/g,'ガ');
+	enhavo = enhavo.replace(/[Gg][Ii]/g,'ギ');
+	enhavo = enhavo.replace(/[Gg][Uu]/g,'グ');
+	enhavo = enhavo.replace(/[Gg][Ee]/g,'ゲ');
+	enhavo = enhavo.replace(/[Gg][Oo]/g,'ゴ');
+	enhavo = enhavo.replace(/[Gg]/g,'ㇰ゛');
+
+	enhavo = enhavo.replace(/[Ĝĝ][Aa]/g,'ヂャ');
+	enhavo = enhavo.replace(/[Ĝĝ][Ii]/g,'ヂ');
+	enhavo = enhavo.replace(/[Ĝĝ][Uu]/g,'ヂュ');
+	enhavo = enhavo.replace(/[Ĝĝ][Ee]/g,'ヂェ');
+	enhavo = enhavo.replace(/[Ĝĝ][Oo]/g,'ヂョ');
+	enhavo = enhavo.replace(/[Ĝĝ]/g,'ㇱ゛');
+
+	enhavo = enhavo.replace(/[Hh][Aa]/g,'ハ');
+	enhavo = enhavo.replace(/[Hh][Ii]/g,'ヒ');
+	enhavo = enhavo.replace(/[Hh][Uu]/g,'ホゥ');
+	enhavo = enhavo.replace(/[Hh][Ee]/g,'ヘ');
+	enhavo = enhavo.replace(/[Hh][Oo]/g,'ホ');
+	enhavo = enhavo.replace(/[Hh]/g,'ㇹ');
+
+	enhavo = enhavo.replace(/[Ĥĥ][Aa]/g,'ッハ');
+	enhavo = enhavo.replace(/[Ĥĥ][Ii]/g,'ッヒ');
+	enhavo = enhavo.replace(/[Ĥĥ][Uu]/g,'ッホウ');
+	enhavo = enhavo.replace(/[Ĥĥ][Ee]/g,'ッヘ');
+	enhavo = enhavo.replace(/[Ĥĥ][Oo]/g,'ッホ');
+	enhavo = enhavo.replace(/[Ĥĥ]/g,'ッㇹ');
+
+	enhavo = enhavo.replace(/[Jj][Aa]/g,'ヤ');
+	enhavo = enhavo.replace(/[Jj][Ii]/g,'イィ');
+	enhavo = enhavo.replace(/[Jj][Uu]/g,'ユ');
+	enhavo = enhavo.replace(/[Jj][Ee]/g,'イェ');
+	enhavo = enhavo.replace(/[Jj][Oo]/g,'ヨ');
+	enhavo = enhavo.replace(/[Jj]/g,'ィ');
+
+	enhavo = enhavo.replace(/[Ĵĵ][Aa]/g,'ジャ');
+	enhavo = enhavo.replace(/[Ĵĵ][Ii]/g,'ジ');
+	enhavo = enhavo.replace(/[Ĵĵ][Uu]/g,'ジュ');
+	enhavo = enhavo.replace(/[Ĵĵ][Ee]/g,'ジェ');
+	enhavo = enhavo.replace(/[Ĵĵ][Oo]/g,'ジョ');
+	enhavo = enhavo.replace(/[Ĵĵ]/g,'ㇱ゛');
+
+	enhavo = enhavo.replace(/[Kk][Aa]/g,'カ');
+	enhavo = enhavo.replace(/[Kk][Ii]/g,'キ');
+	enhavo = enhavo.replace(/[Kk][Uu]/g,'ク');
+	enhavo = enhavo.replace(/[Kk][Ee]/g,'ケ');
+	enhavo = enhavo.replace(/[Kk][Oo]/g,'コ');
+	enhavo = enhavo.replace(/[Kk]/g,'ㇰ');
+
+	enhavo = enhavo.replace(/[Ll][Aa]/g,'ラ゚');
+	enhavo = enhavo.replace(/[Ll][Ii]/g,'リ゚');
+	enhavo = enhavo.replace(/[Ll][Uu]/g,'ル゚');
+	enhavo = enhavo.replace(/[Ll][Ee]/g,'レ゚');
+	enhavo = enhavo.replace(/[Ll][Oo]/g,'ロ゚');
+	enhavo = enhavo.replace(/[Ll]/g,'ㇽ゚');
+
+	enhavo = enhavo.replace(/[Mm][Aa]/g,'マ');
+	enhavo = enhavo.replace(/[Mm][Ii]/g,'ミ');
+	enhavo = enhavo.replace(/[Mm][Uu]/g,'ム');
+	enhavo = enhavo.replace(/[Mm][Ee]/g,'メ');
+	enhavo = enhavo.replace(/[Mm][Oo]/g,'モ');
+	enhavo = enhavo.replace(/[Mm]/g,'ㇺ');
+
+	enhavo = enhavo.replace(/[Nn][Aa]/g,'ナ');
+	enhavo = enhavo.replace(/[Nn][Ii]/g,'ニ');
+	enhavo = enhavo.replace(/[Nn][Uu]/g,'ヌ');
+	enhavo = enhavo.replace(/[Nn][Ee]/g,'ネ');
+	enhavo = enhavo.replace(/[Nn][Oo]/g,'ノ');
+	enhavo = enhavo.replace(/[Nn]/g,'ン');
+
+	enhavo = enhavo.replace(/[Pp][Aa]/g,'パ');
+	enhavo = enhavo.replace(/[Pp][Ii]/g,'ピ');
+	enhavo = enhavo.replace(/[Pp][Uu]/g,'プ');
+	enhavo = enhavo.replace(/[Pp][Ee]/g,'ペ');
+	enhavo = enhavo.replace(/[Pp][Oo]/g,'ポ');
+	enhavo = enhavo.replace(/[Pp]/g,'ㇷ゚');
+
+	enhavo = enhavo.replace(/[Rr][Aa]/g,'ラ');
+	enhavo = enhavo.replace(/[Rr][Ii]/g,'リ');
+	enhavo = enhavo.replace(/[Rr][Uu]/g,'ル');
+	enhavo = enhavo.replace(/[Rr][Ee]/g,'レ');
+	enhavo = enhavo.replace(/[Rr][Oo]/g,'ロ');
+	enhavo = enhavo.replace(/[Rr]/g,'ㇽ');
+
+	enhavo = enhavo.replace(/[Ss][Aa]/g,'サ');
+	enhavo = enhavo.replace(/[Ss][Ii]/g,'スィ');
+	enhavo = enhavo.replace(/[Ss][Uu]/g,'ス');
+	enhavo = enhavo.replace(/[Ss][Ee]/g,'セ');
+	enhavo = enhavo.replace(/[Ss][Oo]/g,'ソ');
+	enhavo = enhavo.replace(/[Ss]/g,'ㇲ');
+
+	enhavo = enhavo.replace(/[Ŝŝ][Aa]/g,'シャ');
+	enhavo = enhavo.replace(/[Ŝŝ][Ii]/g,'シ');
+	enhavo = enhavo.replace(/[Ŝŝ][Uu]/g,'シュ');
+	enhavo = enhavo.replace(/[Ŝŝ][Ee]/g,'シェ');
+	enhavo = enhavo.replace(/[Ŝŝ][Oo]/g,'ショ');
+	enhavo = enhavo.replace(/[Ŝŝ]/g,'ㇱ');
+
+	enhavo = enhavo.replace(/[Tt][Aa]/g,'タ');
+	enhavo = enhavo.replace(/[Tt][Ii]/g,'ティ');
+	enhavo = enhavo.replace(/[Tt][Uu]/g,'ト゚');
+	enhavo = enhavo.replace(/[Tt][Ee]/g,'テ');
+	enhavo = enhavo.replace(/[Tt][Oo]/g,'ト');
+	enhavo = enhavo.replace(/[Tt]/g,'ㇳ');
+
+	enhavo = enhavo.replace(/[Ŭŭ][Aa]/g,'ワ');
+	enhavo = enhavo.replace(/[Ŭŭ][Ii]/g,'ヰ');
+	enhavo = enhavo.replace(/[Ŭŭ][Uu]/g,'于');
+	enhavo = enhavo.replace(/[Ŭŭ][Ee]/g,'ヱ');
+	enhavo = enhavo.replace(/[Ŭŭ][Oo]/g,'ヲ');
+	enhavo = enhavo.replace(/[Ŭŭ]/g,'ゥ');
+
+	enhavo = enhavo.replace(/[Vv][Aa]/g,'ヷ');
+	enhavo = enhavo.replace(/[Vv][Ii]/g,'ヸ');
+	enhavo = enhavo.replace(/[Vv][Uu]/g,'ヴ');
+	enhavo = enhavo.replace(/[Vv][Ee]/g,'ヹ');
+	enhavo = enhavo.replace(/[Vv][Oo]/g,'ヺ');
+	enhavo = enhavo.replace(/[Vv]/g,'ゥ゛');
+
+	enhavo = enhavo.replace(/[Zz][Aa]/g,'ザ');
+	enhavo = enhavo.replace(/[Zz][Ii]/g,'ズィ');
+	enhavo = enhavo.replace(/[Zz][Uu]/g,'ズ');
+	enhavo = enhavo.replace(/[Zz][Ee]/g,'ゼ');
+	enhavo = enhavo.replace(/[Zz][Oo]/g,'ゾ');
+	enhavo = enhavo.replace(/[Zz]/g,'ㇲ゛');
+
+	enhavo = enhavo.replace(/[Aa]/g,'ア');
+	enhavo = enhavo.replace(/[Ii]/g,'イ');
+	enhavo = enhavo.replace(/[Uu]/g,'ウ');
+	enhavo = enhavo.replace(/[Ee]/g,'エ');
+	enhavo = enhavo.replace(/[Oo]/g,'オ');
+
+	enhavo = enhavo.replace(/\ /g,'　');
+	enhavo = enhavo.replace(/\,/g,'、');
+	enhavo = enhavo.replace(/\./g,'。');
+	enhavo = enhavo.replace(/\?/g,'？');
+	enhavo = enhavo.replace(/\!/g,'！');
+
+	enhavo = enhavo.replace(/[Xx]/g,'?');
+
+	document.getElementById('enhavo').value = enhavo;
+}
+
 function malgranda() {
 	tiparaGrandeco--;
 	if (tiparaGrandeco <= 0) {
